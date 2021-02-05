0bfac59ec70daa467412cd024cd11bfca58086c9
josmatoje <josemariamataojeda@gmail.com>
2021-02-05 10:13:27 +0100

    Boletines 8

---
+++ Tema 8/Boletin 8.2.sql
@@ -0,0 +1,14 @@
+USE pubs
+--1. Numero de libros que tratan de cada tema
+
+--2. Número de autores diferentes en cada ciudad y estado
+
+--3. Nombre, apellidos, nivel y antigüedad en la empresa de los empleados con un nivel entre 100 y 150.
+
+--4. Número de editoriales en cada país. Incluye el país.
+
+--5. Número de unidades vendidas de cada libro en cada año (title_id, unidades y año).
+
+--6. Número de autores que han escrito cada libro (title_id y numero de autores).
+
+--7. ID, Titulo, tipo y precio de los libros cuyo adelanto inicial (advance) tenga un valor superior a $7.000, ordenado por tipo y título
