7b45b12f08e7b9867dfd8424780e352bc29ccbf3
josmatoje <josemariamataojeda@gmail.com>
2021-02-09 14:31:27 +0100

    rehecho en clase

--- Tema 8/Boletin 8.1.sql
+++ Tema 8/Boletin 8.1.sql
@@ -23,16 +23,23 @@ GROUP BY CustomerID, YEAR(OrderDate)
 --5. ID del producto, precio unitario y total facturado de ese producto, ordenado por cantidad facturada de mayor a menor. Si hay varios 
 --precios unitarios para el mismo producto tomaremos el mayor.
 
-SELECT ProductID, SUM(Quantity)  FROM [Order Details]
+SELECT ProductID, SUM(Quantity*UnitPrice*(1-Discount)) AS [Total facturado] FROM [Order Details]
 GROUP BY ProductID
+SELECT * FROM [Order Details]
 
 --6. ID del proveedor e importe total del stock acumulado de productos correspondientes a ese proveedor.
-
+SELECT * FROM Products
 --7. Número de pedidos registrados mes a mes de cada año.
 
 --8. Año y tiempo medio transcurrido entre la fecha de cada pedido (OrderDate) y la fecha en la que lo hemos enviado (ShipDate), en días 
 --para cada año.
 
+SELECT YEAR(OrderDate) AS Año, AVG(DATEDIFF(DAY, OrderDate, ShippedDate)) AS [Dias de media] FROM Orders
+GROUP BY YEAR(OrderDate)
+
 --9. ID del distribuidor y número de pedidos enviados a través de ese distribuidor.
 
 --10. ID de cada proveedor y número de productos distintos que nos suministra.
+SELECT SupplierID, COUNT(DISTINCT ProductID) AS [Nº productos] FROM Products
+GROUP BY SupplierID
+
--- Tema 8/Boletin 8.2.sql
+++ Tema 8/Boletin 8.2.sql
@@ -1,14 +1,24 @@
 USE pubs
 --1. Numero de libros que tratan de cada tema
-
+SELECT type, COUNT(*) AS [Nº de libros] FROM titles
+GROUP BY type
 --2. Número de autores diferentes en cada ciudad y estado
-
+SELECT state,city, COUNT(*) AS [Nº de autores] FROM authors
+GROUP BY state,city
+ORDER BY state,city
 --3. Nombre, apellidos, nivel y antigüedad en la empresa de los empleados con un nivel entre 100 y 150.
-
+SELECT fname,lname, job_lvl FROM employee
+WHERE job_lvl BETWEEN 100 AND 150
 --4. Número de editoriales en cada país. Incluye el país.
-
+SELECT ISNULL(state,'--') AS Pais, COUNT(*) AS [Nº de paises] FROM publishers
+GROUP BY state
 --5. Número de unidades vendidas de cada libro en cada año (title_id, unidades y año).
-
+SELECT title_id, SUM(qty) AS Cantidad, YEAR(ord_date) AS Año FROM sales
+GROUP BY title_id, YEAR(ord_date)
+SELECT * FROM sales
 --6. Número de autores que han escrito cada libro (title_id y numero de autores).
 
 --7. ID, Titulo, tipo y precio de los libros cuyo adelanto inicial (advance) tenga un valor superior a $7.000, ordenado por tipo y título
+SELECT title_id, title, type, price FROM titles
+WHERE advance>7000
+ORDER BY type,title
